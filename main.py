18e77edf05f56c5311fc3046f2997eaee60b1e4f
Jonathan <jonrullo@gmail.com>
2026-09-22 13:44:10 -0400

    added actual astreroids

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import asteroidfield
 from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,7 +13,11 @@ def main():
     dt = 0.0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
+    asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
